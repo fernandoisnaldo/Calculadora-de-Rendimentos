9072f68d97bdb23256061b473ad62716fef51a85
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2020-12-10 00:02:41 +0000

    Add files via upload

---
+++ calculadora_de_rendimentos.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+print "Defina o valor de investimento inicial: R\$ ";
+my $investimento_inicial=<>;
+$investimento_inicial=~s/,/./;
+print "Defina o tempo de investimento(meses): -> ";
+my $prazo=<>;
+print "Quanto você pretende depositar, em valores mensais?: R\$ ";
+my $aporte_mensal=<>;
+$aporte_mensal=~s/,/./;
+print "Defina a taxa de rendimento: (anual) % ";
+my $rendimento=<>;
+$rendimento=~s/,/./;
+my $resultado=$investimento_inicial;
+for(my $contador=0;$contador<$prazo;$contador++){
+	$resultado=$resultado*(1+$rendimento/100)**(1/12)+$aporte_mensal;
+}
+print "Você terá R\$ $resultado\n";
+exit;
